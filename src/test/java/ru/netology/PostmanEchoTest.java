e330e96b76f57dc1abcc68cfc2b1bcc309998673
Denis Skidan <skidan147@gmail.com>
2023-09-13 15:21:05 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnChangedData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .log().all()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Василий") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+                .prettyPeek()
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Василий"))
+        ;
+    }
+}
