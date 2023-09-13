d17871c8e57650b0f4268c4943ffcea484cade25
Denis Skidan <skidan147@gmail.com>
2023-09-13 15:31:59 +0300

    test improved

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -23,7 +23,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("Василий"))
         ;
     }
 }
